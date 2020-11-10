4a1fa64ec8ecf4970bd28b8e116f9ea19e9d1f26
Ardahan Kisbet <ardahankisbet@gmail.com>
2020-11-10 22:53:36 +0300

    First Non Repeating Character Algorithm has been added.

--- CommonLib/UtilityCls.cpp
+++ CommonLib/UtilityCls.cpp
@@ -1,6 +1,6 @@
 #include "UtilityCls.h"
 #include <cstring> 
-#include <map>
+#include <unordered_map>
 
 // Trim Operation
 void UtilityCls::Trim(char* src, size_t length)
@@ -62,13 +62,40 @@ void UtilityCls::Trim(char* src, size_t length)
 }
 
 // Finding First Non-Repeating Character In Given String
-char FirstNonRepeatingCharacter(char* src, size_t length)
+// There is two seperate for loop for given source which leads to O(2N), hence O(N) Complexity
+char UtilityCls::FirstNonRepeatingCharacter(const char* src, size_t length)
 {
+	char result = '_';
+
 	// Using Key-Value Pair is definetly more efficent in constrast to using brute-force way by doing 2 loops
-	std::map<char, int> hashMap;
-	hashMap.insert(std::pair<char, int>('a', 0));
+	std::unordered_map<char, int> uMap;
+	
+	for (size_t i = 0; i < length; ++i)
+	{
+		// unordered map O(1) lookup
+		if (uMap[src[i]])
+		{
+			// if hash map has this key then increase count value
+			++uMap[src[i]];
+		}
+		else
+		{
+			// add ne new pair to map
+			uMap[src[i]] = 1;
+		}
+	}
 
-	// TODO
+	// Now our each character and count of them are ready
+	// get first un-repeated one
+	for (size_t i = 0; i < length; ++i)
+	{
+		if (uMap[src[i]] == 1)
+		{
+			result = src[i];
+			// break at first occurence
+			break;
+		}
+	}
 
-	return '_';
+	return result;
 }
--- CommonLib/UtilityCls.h
+++ CommonLib/UtilityCls.h
@@ -3,5 +3,6 @@ class UtilityCls
 {
 public:
 	void Trim(char* src, size_t length);
+	char FirstNonRepeatingCharacter(const char* src, size_t length);
 };
 
