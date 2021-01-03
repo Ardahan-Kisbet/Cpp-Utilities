e052134c6399b992001bd8eb6cfdc765437ce597
Ardahan Kisbet <ardahankisbet@gmail.com>
2021-01-03 14:41:53 +0300

    Solved Length of Last Word Problem (Easy)

--- CommonLib/UtilityCls.cpp
+++ CommonLib/UtilityCls.cpp
@@ -99,3 +99,37 @@ char UtilityCls::FirstNonRepeatingCharacter(const char* src, size_t length)
 
 	return result;
 }
+
+// Length of Last Word
+int UtilityCls::lengthOfLastWord(std::string s)
+{
+	int counter = 0;
+	bool spaceEncountered = false;
+
+	for (size_t i = 0; i < s.size(); ++i)
+	{
+		if ((s[i] == ' ') && (spaceEncountered == false))
+		{
+			// Set space encountered flag to true
+			spaceEncountered = true;
+		}
+		else if ((s[i] != ' ') && (spaceEncountered == false))
+		{
+			// Then start/continue counting
+			++counter;
+		}
+		else if ((s[i] != ' ') && (spaceEncountered == true))
+		{
+			// then we encountered with new word
+			// reset counter
+			// new word's first letter encountered
+			counter = 1;
+			// set space encountered flag to false
+			spaceEncountered = false;
+		}
+		// Else: consecutive white spaces, ignore it
+	}
+
+	return counter;
+}
+
--- CommonLib/UtilityCls.h
+++ CommonLib/UtilityCls.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <string>
 class UtilityCls
 {
 public:
 	void Trim(char* src, size_t length);
 	char FirstNonRepeatingCharacter(const char* src, size_t length);
+	int lengthOfLastWord(string s);
 };
 
