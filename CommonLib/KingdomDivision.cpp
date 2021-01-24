1866010003742e379277ffef9af8cab83ec78b59
Ardahan Kisbet <ardahankisbet@gmail.com>
2021-01-24 15:47:37 +0300

    Performance enhancement which leads to 100% Score on Codility

--- CommonLib/KingdomDivision.cpp
+++ CommonLib/KingdomDivision.cpp
@@ -1,11 +1,10 @@
 #include <vector>
+#include <unordered_map>
 
-// 50% performance 100% correctness
+// %100 Performance & Task Score & Correctness
 // Codility: In how many ways can you split a kingdom into two parts, so that the parts contain equal number of gold mines ?
-int DividingTheKingdom(int N, int M, std::vector<int>& X, std::vector<int>& Y) {
+int solution(int N, int M, std::vector<int>& X, std::vector<int>& Y) {
 	// write your code in C++14 (g++ 6.2.0)
-
-	// 78% Task Score 100% correctness %50 Performance
 
 	if (X.size() % 2 != 0)
 	{
@@ -15,18 +14,31 @@ int DividingTheKingdom(int N, int M, std::vector<int>& X, std::vector<int>& Y) {
 
 	int res = 0;
 
+	// store all column/row values by counting them only once
+	// they will be used below when we are searching needed gold boroughs
+	std::unordered_map<int, int> umapY, umapX;
+	for (int i : Y)
+	{
+		++umapY[i];
+	}
+
+	for (int i : X)
+	{
+		++umapX[i];
+	}
+
 	// how many gold boroughs we are looking for
 	// either half size of X or Y vector
 	int neededGoldCount = X.size() / 2;
 
 	// if vertical column (M) count is 1 then there is no need to check
-	bool yConsistOneColumn = std::count(Y.begin(), Y.end(), 0) == static_cast<int>(Y.size());
+	bool yConsistOneColumn = umapY[0] == static_cast<int>(Y.size());
 	if (!yConsistOneColumn)
 	{
 		int temp = 0;
 		for (auto i = 0; i < M; ++i)
 		{
-			temp += std::count(Y.begin(), Y.end(), i);
+			temp += umapY[i];
 
 			if (temp == neededGoldCount)
 			{
@@ -42,13 +54,13 @@ int DividingTheKingdom(int N, int M, std::vector<int>& X, std::vector<int>& Y) {
 	}
 
 	// if horizontal column (N) count is 1 then there is no need to check
-	bool xConsistOneColumn = std::count(X.begin(), X.end(), 0) == static_cast<int>(X.size());
+	bool xConsistOneColumn = umapX[0] == static_cast<int>(X.size());
 	if (!xConsistOneColumn)
 	{
 		int temp = 0;
 		for (auto i = 0; i < N; ++i)
 		{
-			temp += std::count(X.begin(), X.end(), i);
+			temp += umapX[i];
 
 			if (temp == neededGoldCount)
 			{
